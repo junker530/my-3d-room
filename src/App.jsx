7865158d55dc49a8a2c611226ec86596663862db
junker530 <ddr4646@gmail.com>
2026-02-26 07:30:55 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import '@google/model-viewer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="viewer-container">
+      <model-viewer
+        src="./room.glb"
+        camera-controls
+        auto-rotate
+        shadow-intensity="1"
+        alt="Scaniverse 3D Room"
+      ></model-viewer>
+    </div>
+  );
+}
+
+export default App;
